3e6e0c8bb0472a74db031fdf6b9102ec531b4243
pkaho <pkaho@qq.com>
2026-02-02 17:26:17 +0800

    use Jetbrain as default font

--- dot_config/wezterm/create_override.lua
+++ dot_config/wezterm/create_override.lua
@@ -12,21 +12,27 @@ local schemes = {
     }
 }
 
+local fontlist = wezterm.font_with_fallback({
+    -- en
+    { family = "JetBrainsMono Nerd Font Mono", scale = 1.0 },
+    { family = "Annotation Mono",              scale = 1.0 },
+    { family = "SF Mono",                      scale = 1.0 },
+    { family = "MonoLisa",                     scale = 1.0 },
+    { family = "TX-02",                        scale = 1.0 },
+    { family = "CodeNewRoman Nerd Font",       scale = 1.1 },
+    { family = "FiraCode Nerd Font Mono",      scale = 1.0 },
+    { family = "Iosevka Nerd Font Mono",       scale = 1.1 },
+    { family = "Maple Mono NF",                scale = 1.0 },
+    { family = "RobotoMono Nerd Font",         scale = 1.0 },
+    { family = "SauceCodePro Nerd Font",       scale = 1.0 },
+    { family = "SpaceMono Nerd Font",          scale = 1.0 },
+    -- cn
+    { family = "LXGW Neo XiHei",               scale = 1.0 },
+    { family = "Maple Mono NF CN",             scale = 1.0 },
+    { family = "LXGW WenKai",                  scale = 1.0 },
+})
+
 return function(config)
-    -- config.color_scheme = schemes.dark[5]
-    config.font = wezterm.font_with_fallback({
-        -- en
-        { family = "RobotoMono Nerd Font",         scale = 1.0 },
-        { family = "SF Mono",                      scale = 1.0 },
-        { family = "MonoLisa",                     scale = 1.0 },
-        { family = "TX-02",                        scale = 1.0 },
-        { family = "JetBrainsMono Nerd Font Mono", scale = 1.0 },
-        { family = "Maple Mono NF",                scale = 1.0 },
-        { family = "FiraCode Nerd Font Mono",      scale = 1.0 },
-        { family = "Iosevka Nerd Font Mono",       scale = 1.1 },
-        -- cn
-        { family = "LXGW Neo XiHei",               scale = 1.0 },
-        { family = "Maple Mono NF CN",             scale = 1.0 },
-        { family = "LXGW WenKai",                  scale = 1.0 },
-    })
+    config.color_scheme = schemes.dark[5]
+    config.font = fontlist
 end
