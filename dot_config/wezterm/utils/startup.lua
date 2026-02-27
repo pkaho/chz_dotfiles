e2f74e49462b0333a42f3dd692c8935910d8c90e
pkaho <pkaho@qq.com>
2026-02-27 17:42:06 +0800

    fix open with wezterm not in current folder

--- dot_config/wezterm/utils/startup.lua
+++ dot_config/wezterm/utils/startup.lua
@@ -1,15 +1,20 @@
 local wezterm = require('wezterm')
 
 wezterm.on("gui-startup", function(cmd)
+    local cwd = cmd and cmd.cwd or nil
     local screen = wezterm.gui.screens().active
-    local ratio = 0.5
-    local width, height = screen.width * ratio, screen.height * ratio
-    local tab, pane, window = wezterm.mux.spawn_window {
-        position = {
-            x = (screen.width - width) / 2,
-            y = (screen.height - height) / 2,
-            origin = 'ActiveScreen' }
-    }
-    -- window:gui_window():maximize()
-    window:gui_window():set_inner_size(width, height)
+    local ratio = 0.5               -- 窗口占当前屏幕的比例
+    local w = screen.width * ratio  -- 窗口宽度
+    local h = screen.height * ratio -- 窗口高度
+
+    -- 窗口继承工作目录
+    local _, _, window = wezterm.mux.spawn_window { cwd = cwd }
+
+    local win = window:gui_window()
+    win:set_inner_size(w, h)
+    win:set_position(
+        (screen.width - w) / 2,
+        (screen.height - h) / 2,
+        'ActiveScreen'
+    )
 end)
